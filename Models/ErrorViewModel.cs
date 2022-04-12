2f20b54c80538f0a5e5d04bdcbf42582d76081a6
Diego Marcos <diego.marcos.ext@trba.pe>
2022-04-12 11:17:35 -0500

    primeros cambios

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Produccion.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
